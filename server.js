8e6d44f880116f073a6abd424298cd32c9282d7f
Lucia <luciag.lara@gmail.com>
2021-05-20 09:10:28 +0200

    correccion

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const playwright = require('playwright');
   const page = await context.newPage();
 
   await page.goto('https://web.gencat.cat/ca/inici', {waitUntil: 'load', timeout: 0 } ); // he tenido que añadir estos eventos ya que me daba problemas la carga de la página excediéndose su tiempo de ejecución 
-  await page.fill('#cercadorOcultGoogle', 'agenda aultural'); 
+  await page.fill('#cercadorOcultGoogle', 'agenda cultural'); 
   await page.click('[aria-label="Cercar"]');
   await page.waitForTimeout ( 3000 ); // he añadido este timeout para que no se cierre de manera inmediata
   await browser.close();
